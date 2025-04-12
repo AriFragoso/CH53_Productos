96c60fa3569414ffc75259130a50cbd1b78507e4
afr <arifragoso666@yahoo.com.mx>
2025-04-11 20:16:28 -0600

    ultimas actializaciones vistas

--- JS/prueba.js
+++ JS/prueba.js
@@ -1,13 +1,16 @@
 const main = document.getElementsByTagName("main").item(0);
+let mainProds = document.getElementById("mainProds");
+const ulMenu = document.getElementById("ulMenu");
 const URLMain = "https://fakestoreapi.com/products/";
 
-function getData(){
-    fetch(URLMain)
+
+function getData(cat){
+    fetch(URLMain+cat)
     .then((response) => {
         console.log(response);
         response.json().then((res)=>{
-            console.log(res.length); //20
-            console.log(res[0].title);
+            //console.log(res.length); //20
+            //console.log(res[0].title);
             createCards(res);
         });
     })
@@ -18,11 +21,36 @@ function getData(){
             </div>`);
     });
 }//getData
-getData();
 
-function createCards(prods) {
-    prods.forEach(res => {
+
+
+function getCategories(){
+    fetch(URLMain+"categories/")
+    .then((response) => {
+        console.log(response);
+        response.json().then((res)=>{
+            //console.log("categories:",res);
+            res.forEach((cat)=>{
+                ulMenu.insertAdjacentHTML("afterbegin",
+                `<li><a class="dropdown-item" onclick="getData('category/${cat}');">${cat}</a></li>`);
+            })
+        });
+    })
+    .catch((err)=>{
         main.insertAdjacentHTML("beforeend",
+            `<div class="alert alert-danger" role="alert">
+               ${err.message}
+               </div>`);
+            });
+}//getCategories
+getCategories();
+getData("");
+
+function createCards(products) {
+    mainProds.innerHTML="";
+    products.forEach(p => {
+        console.log(p.id,p.title, p.price);
+        mainProds.insertAdjacentHTML("beforeend",
             `<div class="card" style="width: 18rem;">
                 <img src="${res.image}" class="card-img-top" alt="${res.title}">
                 <div class="card-body">
@@ -31,5 +59,8 @@ function createCards(prods) {
                     <a href="#" class="btn btn-primary">Go somewhere</a>
                 </div>
             </div>`);
-    });
+    })
 }//createCard: ciclo que pueda ir recorriendo los productos y los muestre 
+createCards();
+
+
